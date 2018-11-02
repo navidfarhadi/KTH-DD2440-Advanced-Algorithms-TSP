1e16f5c55b286be9589d11e567f952802ddd7d90
Franz <franz.fuchs97@gmx.de>
2018-11-02 11:45:38 +0100

    improved prim algorithm implementation

--- mst.cpp
+++ mst.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <cstdlib>
 #include <tuple>
+#include <limits>
+#include <cmath>
 
 /*
  * This is the file that implements the minimum spanning tree
@@ -17,19 +19,14 @@ class MST
         void getMST(std::vector<std::array<int,2>> &e);
         std::vector<int> parent;
         std::vector<std::array<double,2>> vertices;
-        void sortEdges(std::vector<std::array<int,2>> &e);
         int comparator(std::vector<std::array<int,2>> &e1, std::vector<std::array<int,2>> &e2);
-
+        double computeEdgeDist(int ind1, int ind2);
+        double computeDistance(std::array<double,2> v1, std::array<double,2> v2);
 };
 
 MST::MST()
 {
     std::cout << "MST\n";
-}
-
-void MST::sortEdges(std::vector<std::array<int,2>> &e)
-{
-    //std::sort(e.begin,e.end,comparator);
 }
 
 int MST::comparator(std::vector<std::array<int,2>> &e1, std::vector<std::array<int,2>> &e2)
@@ -41,26 +38,86 @@ void MST::getMST(std::vector<std::array<int,2>> &e)
 {
     int size = vertices.size();
     double dist[size];
-    double pred[size];
+    int pred[size];
+    bool visited[size] = {false};
     // we always start with vertic index 0
     // because it does not make a difference
     
     // distance zero
     dist[0] = 0;
-    // no predecessor
-    pred[0] = -1;
-
+    // all other have max distance
+    for(int i = 1; i < size; i++)
+    {
+        dist[i] = __DBL_MAX__;
+    }
+    // -2: root node
+    // -1: no predecessor up to now
+    pred[0] = -2;
+    for(int i = 1; i < size; i++)
+    {
+        pred[i] = -1;
+    }
     // boolean value stands for if it is invalid
     // true: invalid
     // false: valid
-    std::vector<std::tuple<int,double,bool>> pq;
+    std::vector<std::pair<int,double>> pq;
 
-    pq.push_back(std::make_tuple(0,0,false));
+    pq.push_back({0,0});
 
     std::make_heap(pq.begin(),pq.end());
+
+    while (!pq.empty())
+    {
+        std::pair<int,double> v = pq.front();
+        pq.erase(pq.begin());
+        if(visited[v.first])
+        {
+            // we have already visited that one
+            // so we can abort the current iteration
+            continue;
+        }
+        else
+        {
+            // mark it as visited
+            visited[v.first] = true;
+        }
+
+        for(int w = 0; w < size; w++)
+        {
+            if(!visited[w])
+            {
+                double newDist = computeEdgeDist(v.first,w);
+                if(newDist < dist[w])
+                {
+                    pred[w] = v.first;
+                    pq.push_back({w,newDist});
+                    dist[w] = newDist;
+                    // we changed the heap so we need to build it again
+                    std::make_heap(pq.begin(),pq.end());
+                }
+            }
+        }
+    }
 }
+
+double MST::computeEdgeDist(int ind1, int ind2)
+{
+    return computeDistance(vertices[ind1],vertices[ind2]);
+}
+
+// computes the euclidian distance between two points
+// no rounding is made as an adjustment for the christofides algorithm
+double MST::computeDistance(std::array<double,2> v1, std::array<double,2> v2)
+{
+    double sum = (v1.at(0) - v2.at(0))*(v1.at(0) - v2.at(0));
+    sum += (v1.at(1) - v2.at(1))*(v1.at(1) - v2.at(1));
+    sum = std::sqrt(sum);
+    return sum;
+}
+
 int main()
 {
     MST *m = new MST();
     m->parent[0];
+    std::cout << __DBL_MAX__ << "\n";
 }

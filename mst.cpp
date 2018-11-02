c7c2630fc459a27aceaf013bfd178c82a9d0475b
Franz <franz.fuchs97@gmx.de>
2018-11-02 10:14:41 +0100

    started implementing the algorithm of prim

--- mst.cpp
+++ mst.cpp
@@ -2,17 +2,19 @@
 #include <vector>
 #include <array>
 #include <algorithm>
+#include <cstdlib>
+#include <tuple>
 
 /*
  * This is the file that implements the minimum spanning tree
- * algorithm. We use the algorithm of Kruskal
+ * algorithm. We use the algorithm of Prim
  */
 
 class MST
 {
     public:
         MST();
-        std::vector<std::array<int,2>> getMST();
+        void getMST(std::vector<std::array<int,2>> &e);
         std::vector<int> parent;
         std::vector<std::array<double,2>> vertices;
         void sortEdges(std::vector<std::array<int,2>> &e);
@@ -35,6 +37,28 @@ int MST::comparator(std::vector<std::array<int,2>> &e1, std::vector<std::array<i
     return 0;
 }
 
+void MST::getMST(std::vector<std::array<int,2>> &e)
+{
+    int size = vertices.size();
+    double dist[size];
+    double pred[size];
+    // we always start with vertic index 0
+    // because it does not make a difference
+    
+    // distance zero
+    dist[0] = 0;
+    // no predecessor
+    pred[0] = -1;
+
+    // boolean value stands for if it is invalid
+    // true: invalid
+    // false: valid
+    std::vector<std::tuple<int,double,bool>> pq;
+
+    pq.push_back(std::make_tuple(0,0,false));
+
+    std::make_heap(pq.begin(),pq.end());
+}
 int main()
 {
     MST *m = new MST();

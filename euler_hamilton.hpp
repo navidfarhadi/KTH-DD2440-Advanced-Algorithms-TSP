0af0bf1171e504a72ec667515cee0fd61750ad10
Navid Farhadi <navid@navidfarhadi.com>
2018-11-11 11:46:03 +0100

    modifying header

--- euler_hamilton.hpp
+++ euler_hamilton.hpp
@@ -2,4 +2,4 @@
 #include <map>
 #include <vector>
 
-void changeToMap(std::vector<std::array<int,2>> original_graph, std::map<int,std::vector<int>> new_graph);
+void changeToMap(std::vector<std::array<int,2>> &original_graph, std::map<int,std::vector<int>> &new_graph);

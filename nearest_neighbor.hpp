9cdff2708e66f55e4cec1bdd19ae21a00124b48f
apeinot <35868146+apeinot@users.noreply.github.com>
2018-12-01 12:26:06 +0100

    Delete nearest_neighbor.hpp

--- nearest_neighbor.hpp
+++
@@ -1,9 +0,0 @@
-/*
- * This is the hpp file for the implementation
- * of the nearest neighbor algorithm
- */
-
-#include <vector>
-#include <array>
-
-void compute_nearest_neighbor(std::vector<int> &path, std::vector<std::array<double,2>> &v);
